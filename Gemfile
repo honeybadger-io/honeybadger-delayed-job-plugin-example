954f9566fe63aef42e502fc9eb7edd010af2d08d
Joshua Wood <josh@hintmedia.com>
2013-02-20 16:21:56 -0800

    initial

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 3.2.11'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+
+gem 'honeybadger'
+gem 'delayed_job'
+gem 'delayed_job_active_record'
+
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'debugger'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoneybadgerDelayedJobExample::Application.config.secret_token = 'e8616f9a4e89466745b94f76159c3773cf0be484846685b2eb99715e6416f72f7d6260af397987e26c8cdc050221fdd2d70a6ddf77c838da314669b06be2cdec'
