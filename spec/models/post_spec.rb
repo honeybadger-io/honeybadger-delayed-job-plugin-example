811e6b3a28f5088d57e3b1d62ba413a1340b069b
Joshua Wood <josh@hintmedia.com>
2014-06-18 09:28:02 -0700

    Use work_off so that plugins are invoked.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 describe Post do
-  before do
-    Delayed::Worker.delay_jobs = false
-  end
+  after { Delayed::Worker.new.work_off }
 
   it "delays .slow_method" do
-    expect(Post.delay.slow_method).to be_a Delayed::Job
+    expect(Post.delay.slow_method(1)).to be_a Delayed::Job
   end
 end
